e605706b5586f6a2b28a9381bb393ab243fb1068
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-05-25 23:41:34 +0000

    🔥Initial Commit

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+window.addEventListener('load', function(){
+  // canvas set up
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 1500;
+  canvas.height = 500;
+})
