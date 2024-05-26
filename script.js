36605d4f768a948ebc8ab6c804dbb49994458b6d
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-05-26 12:14:08 +0000

    a

--- script.js
+++ script.js
@@ -12,7 +12,19 @@ window.addEventListener('load', function(){
     
   }
   class Player {
-    
+    constructor(game){
+      this.game = game;
+      this.width = 120;
+      this.height = 190;
+      this.x = 20;
+      this.y = 100;
+    }
+    update(){
+      this.y += this.speedY;
+    }
+    draw(context){
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
   }
   class Particle {
     
@@ -30,7 +42,10 @@ window.addEventListener('load', function(){
     
   }
   class Game {
-    
+    constructor(width, height){
+      this.width = width;
+      this.height = height;
+    }
   }
 
 
