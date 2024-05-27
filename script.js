899f02c9df351fd48f25fe783984f63964147f5f
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-05-27 14:39:08 +0000

    s

--- script.js
+++ script.js
@@ -9,12 +9,14 @@ window.addEventListener('load', function(){
     constructor(game){
       this.game = game;
       window.addEventListener('keydown', e => {
-        if ((e.key === 'ArrowUp') && this.game.keys.indexOf(e.key) === -1){
+        if ((  (e.key === 'ArrowUp') || 
+               (e.key === 'ArrowDown') 
+        ) && this.game.keys.indexOf(e.key) === -1){
           this.game.keys.push(e.key);
         } 
         console.log(this.game.keys);
       });
-      window.addEventListener('keydown', e => {
+      window.addEventListener('keyup', e =>{
         if(this.game.keys.indexOf(e.key) > -1){
           this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
         }
