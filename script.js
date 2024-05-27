604d65c5c9c74e741835ea53b7ecb0dca2677adf
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-05-27 23:56:21 +0000

    a

--- script.js
+++ script.js
@@ -72,6 +72,7 @@ window.addEventListener('load', function(){
     draw(context){
       context.fillStyle = 'black';
       context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(this.image, this.x, this.y);
       this.projectiles.forEach(projectile => {
         projectile.draw(context);
       });
