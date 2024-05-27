502d4f06bb01fdc1223fb50c5a25ea3350e4860b
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-05-27 16:05:26 +0000

    a

--- script.js
+++ script.js
@@ -95,6 +95,16 @@ window.addEventListener('load', function(){
   class UI {
     constructor(game){
       this.game = game;
+      this.fontSize = 25;
+      this.fontFamily = 'Helvetica';
+      this.color = 'white';
+    }
+    draw(context){
+      // ammo
+      context.fillStyle = this.color;
+      for (let i = 0; i < this.game.ammo; i++){
+        context.fillRect(20, 50, 3, 20);
+      }
     }
   }
   class Game {
