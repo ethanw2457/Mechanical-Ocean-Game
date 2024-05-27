ffdadffea6223652df436d8dc6a423531e437134
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-05-27 14:53:13 +0000

    a

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ window.addEventListener('load', function(){
                (e.key === 'ArrowDown') 
         ) && this.game.keys.indexOf(e.key) === -1){
           this.game.keys.push(e.key);
-        } 
+        } else if ( e.key === ' '){
+          this.game.player.shootTop();
+        }
         console.log(this.game.keys);
       });
       window.addEventListener('keyup', e =>{
@@ -25,7 +27,23 @@ window.addEventListener('load', function(){
     }
   }
   class Projectile {
-    
+    constructor(game, x, y){
+      this.game = game;
+      this.x = x;
+      this.y = y;
+      this.width = 10;
+      this.height = 3;
+      this.speed = 3;
+      this.markedForDeletion = false;
+    }
+    update(){
+      this.x += this.speed;
+      if (this.x > this.game.width * 0.8) this.markedForDeletion = true;
+    }
+    draw(context){
+      context.fillStyle = 'yellow';
+      fillRect(this.x, this.y, this.width, this.height);
+    }
   }
   class Particle {
 
@@ -38,14 +56,25 @@ window.addEventListener('load', function(){
       this.x = 20;
       this.y = 100;
       this.speedY = 0.2;
+      this.maxSpeed = 3;
+      this.projectiles = [];
     }
     update(){
-      if (this.game.keys.includes('ArrowUp')) this.speedY = -1;
-      else if (this.game.keys.includes('ArrowDown')) this.speedY = 1;
+      if (this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
+      else if (this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
+      else this.speedY = 0;
       this.y += this.speedY;
+      // Handle the projectiles
+      this.projectiles.forEach(projectile => {
+        projectile.update();
+      });
     }
     draw(context){
+      context.fillStyle = 'black';
       context.fillRect(this.x, this.y, this.width, this.height);
+    }
+    shootTop(){
+      this.projectiles.push(new Projectile(this.game, this.x, this.y));
     }
   }
   class Enemy {
