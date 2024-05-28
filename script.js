e5d20ac9d2ecde07757f1abeda786b787956d4bd
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-05-28 00:56:24 +0000

    a

--- script.js
+++ script.js
@@ -147,13 +147,14 @@ window.addEventListener('load', function(){
   class LuckyFish extends Enemy {
     constructor(game){
       super(game);
-      this.width = 213;
-      this.height = 165;
+      this.width = 99;
+      this.height = 95;
       this.y = Math.random() * (this.game.height * 0.9 - this.height);
       this.image = document.getElementById('angler2');
       this.frameY = Math.floor(Math.random() * 2);
       this.lives = 3;
-      this.score = this.lives;
+      this.score = 15;
+      this.type = 'lucky';
     }
   }
   
