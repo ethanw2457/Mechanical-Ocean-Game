342cff29f535dea6cc9888b9539571453dbb2a26
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-05-28 00:35:12 +0000

    a

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function(){
   // canvas set up
   const canvas = document.getElementById('canvas1');
   const ctx = canvas.getContext('2d');
-  canvas.width = 500;
+  canvas.width = 700;
   canvas.height = 500;
 
   class InputHandler {
