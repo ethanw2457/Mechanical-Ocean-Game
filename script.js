38b50843fa6207668685046c3f23213e699e96ce
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-05-28 21:38:14 +0000

    c

--- script.js
+++ script.js
@@ -237,6 +237,20 @@ window.addEventListener('load', function(){
       this.speedX = Math.random() * -1.2 - 0.2;
     }
   }
+  class Drone extends Enemy {
+    constructor(game){
+      super(game);
+      this.width = 400;
+      this.height = 227;
+      this.y = Math.random() * (this.game.height * 0.95 - this.height);
+      this.image = document.getElementById('hivewhale');
+      this.frameY = 0;
+      this.lives = 15;
+      this.score = this.lives;
+      this.type = 'hive';
+      this.speedX = Math.random() * -1.2 - 0.2;
+    }
+  }
   
   class Layer {
     constructor (game, image, speedModifier){
