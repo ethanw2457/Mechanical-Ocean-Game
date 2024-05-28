e3afda1dc5f4ce15f06d7107b1bf09b85d636f57
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-05-28 00:51:34 +0000

    2

--- script.js
+++ script.js
@@ -138,8 +138,8 @@ window.addEventListener('load', function(){
       this.width = 213;
       this.height = 165;
       this.y = Math.random() * (this.game.height * 0.9 - this.height);
-      this.image = document.getElementById('angler1');
-      this.frameY = Math.floor(Math.random() * 3);
+      this.image = document.getElementById('angler2');
+      this.frameY = Math.floor(Math.random() * 2);
     }
   }
   
