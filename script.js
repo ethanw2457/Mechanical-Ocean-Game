15f36b2ec3216dbe98fedc4224070ebcadf7cab9
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-05-28 00:03:46 +0000

    a

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ window.addEventListener('load', function(){
       }
     }
     draw(context){
-      context.strokeRect(this.x, this.y, this.width, this.height);
+      if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
       context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
       this.projectiles.forEach(projectile => {
         projectile.draw(context);
