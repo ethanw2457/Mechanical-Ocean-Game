ac5ba596496dc5ea8c05cd38bab59a272985d7b9
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-05-28 00:05:33 +0000

    d

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ window.addEventListener('load', function(){
           this.game.keys.push(e.key);
         } else if ( e.key === ' '){
           this.game.player.shootTop();
-        }
+        } else if (e.key === 'd')
       });
       window.addEventListener('keyup', e =>{
         if(this.game.keys.indexOf(e.key) > -1){
